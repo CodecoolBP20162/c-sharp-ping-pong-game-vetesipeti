d64d4a46d4ed5460e9dbd0c4740fc0d534641acb
vetesipeti <vetesipeti@gmail.com>
2017-06-16 06:15:16 +0200

    half done

---
+++ Pong/Pong/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Paddle = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Field = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Paddle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Field)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Paddle
+            // 
+            this.Paddle.Image = ((System.Drawing.Image)(resources.GetObject("Paddle.Image")));
+            this.Paddle.Location = new System.Drawing.Point(347, 500);
+            this.Paddle.Name = "Paddle";
+            this.Paddle.Size = new System.Drawing.Size(80, 12);
+            this.Paddle.TabIndex = 0;
+            this.Paddle.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Field
+            // 
+            this.Field.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Field.Location = new System.Drawing.Point(12, 12);
+            this.Field.Name = "Field";
+            this.Field.Size = new System.Drawing.Size(747, 509);
+            this.Field.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Field.TabIndex = 1;
+            this.Field.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(325, 182);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(38, 36);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 533);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Paddle);
+            this.Controls.Add(this.Field);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Paddle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Field)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Paddle;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Field;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Pong/Pong/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class Form1 : Form
+    {
+        private bool movingLeft;
+        private bool movingRight;
+        private int pooVelocityX;
+        private int pooVelocityY;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void MovePoo()
+        {
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+
+            if (pictureBox1.Top <= Field.Top | pictureBox1.Bottom >= Field.Bottom)
+            {
+                pooVelocityY *= -1;
+            }
+            else if (pictureBox1.Left <= Field.Left | pictureBox1.Right >= Field.Right)
+            {
+                pooVelocityX *= -1;
+            } else if(pictureBox1.Bounds.IntersectsWith(Paddle.Bounds))
+            {
+                pooVelocityY *= -1;
+            }
+            pictureBox1.Location = new Point(x + pooVelocityX, y + pooVelocityY);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 10;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Start();
+            pooVelocityX = 2;
+            pooVelocityY = 1;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MovePoo();
+            if (movingRight && Paddle.Right < Field.Right - 3)
+            {
+                Paddle.Left += 2;
+            }
+            else if (movingLeft && Paddle.Left > Field.Left + 3)
+            {
+                Paddle.Left -= 2;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) movingRight = true;
+            else if (e.KeyCode == Keys.Left) movingLeft = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) movingRight = false;
+            else if (e.KeyCode == Keys.Left) movingLeft = false;
+        }
+    }
+}
